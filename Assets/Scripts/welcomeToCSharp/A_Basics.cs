9fc1ddfd97cca0c0d6eb47e981c357e280dcda9b
RealBigHindi <max.neuber@googlemail.com>
2022-03-11 22:36:17 +0100

    added A_Basics.cs and A_Variables.cs

---
+++ Assets/Scripts/welcomeToCSharp/A_Basics.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Welcome to CSharp
+
+
+/*
+ * This is a class.
+ * Classes are holding variables and methods.
+ **/
+public class A_Basics
+{
+    // 1. This are variables
+    int integer_1;
+    int integer_2 = 1;
+    // it consists of data type [int], a name [integer_1] and an optional value [= 1]
+
+    // 2. this is a method
+    void myMethod(int iAmOptional)
+    {
+        integer_1 = integer_2:
+    }
+    // it consists of the return type [void] (which means 'no returned value'), the name [myMethod], optional parameters [(int iAmOptional)] and curly braces {which cotain the code}
+}
---
+++ Assets/Scripts/welcomeToCSharp/A_Variables.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This is a class.
+ * Classes are holding variables and methods.
+ **/
+public class A_Variables
+{
+    // 1. Relevant data types
+
+    bool boolean = true;
+    char character = 's';
+    string characterChain = "Hello there";
+    int integer = 1;
+    long doubleSizedInteger = 2l;
+    double doublePrecisionFloat = 1.0;
+
+    // 2. Relevant keywords
+
+    final int cantBeChanged = 1;                                    // final
+    public int canBeSeenFromOtherClasses;                           // public
+    private int canBeSeenOnlyFromThisClass;                         // private
+    protected int canBeSeenFromThisClassesAndItsChildren;           // protected
+}
